34f523cd45013ffc22f76a5add3c9398d0a1ce41
shriyash <shriyashband@gmail.com>
2022-08-04 19:23:24 +0530

    creating folders and schema for backend and frontend

---
+++ api/models/Hospital.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const HospitalSchema = new mongoose.Schema({
+
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    isHospital: { type: Boolean, default: true }
+},
+    { timestamps: true }
+
+);
+
+module.exports = mongoose.model("Hospital", HospitalSchema);
