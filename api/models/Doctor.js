93664b565ae1cbdacbffaed6f6107d459a71af38
shriyash <shriyashband@gmail.com>
2022-08-05 14:23:36 +0530

    resolving conflicts

--- api/models/Doctor.js
+++ api/models/Doctor.js
@@ -4,12 +4,13 @@ const DoctorSchema = new mongoose.Schema({
 
     name: { type: String, required: true },
     license: { type: String, required: true, unique: true },
+    age: { type: String, required: true },
+    gender: { type: String, required: true },
     qualification: { type: String, required: true },
     experience: { type: String, required: true },
     specialization : { type: String, required: true},
     charges : { type : Number },
-    availbility : { type: Array },
-    
+    availbility : { type: Array }
 },
     { timestamps: true }
 
