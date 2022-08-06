b392d50cc9b5d3195d443d7e8a9e8de4a4f18361
shriyash <shriyashband@gmail.com>
2022-08-06 17:48:02 +0530

    creating authentication routes

--- api/index.js
+++ api/index.js
@@ -3,16 +3,28 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const authRoute = require("./routes/auth");
 
 
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 
 }).then(() => console.log(" DB connection successfull")).catch((err) => console.log(err));
 
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+
+
 app.listen(8800, () => {
     console.log("Application running");
-})
+});
+
+
+
+
 module.exports = app;
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,2 +1,60 @@
-const router = require("express").router;
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js")
 
+// Register
+
+router.post("/register", async (req, res) => {
+
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt( req.body.password,process.env.SECRET_KEY).toString(),
+        mobileNo: req.body.mobileNo,
+        age: req.body.age,
+        dob: req.body.dob,
+        gender: req.body.gender,
+        address:req.body.address
+    })
+
+    try {
+        const user = await newUser.save();
+        res.status(201).json(user);
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+
+
+
+//LOGIN
+
+router.post("/login",async (req,res)=>{
+
+try {
+    const user = await User.findOne({username: req.body.username});
+    !user && res.status(400).json("Wrong password or username");
+
+    const bytes = CryptoJS.AES.decrypt(user.password,process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+
+        originalPassword !== req.body.password && 
+        res.status(401).json(("Wrong password or username"));
+
+        const {password, ...info} = user._doc;
+        res.status(200).json(info);
+        
+        
+}catch (error) {
+    res.status(500).json(error);
+}
+
+
+
+});
+
+
+
+module.exports = router;
